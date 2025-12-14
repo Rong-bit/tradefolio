978febe386e4f3441b38ad589ad20f69ab47c407
Rong-bit <hcj382000@gmail.com>
2025-12-15 01:08:52 +0800

    Delete components directory

--- components/components/TransactionList.tsx
+++
@@ -1,9 +0,0 @@
-
-// This component is deprecated and has been merged into App.tsx to provide better integration with history view.
-import React from 'react';
-
-const TransactionList = () => {
-  return <div>Component Deprecated</div>;
-};
-
-export default TransactionList;
